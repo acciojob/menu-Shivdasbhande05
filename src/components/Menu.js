367c0ecb214a3686975fe36bfc657e28166cfb2c
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-02-16 18:26:10 +0000

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ function Menu({ items }) {
     return (
       <div>
         {items.map((item) => (
-          <div key={item.id}>
+          <div key={item.id} data-test-id{`menu-item ${item.category}`}>
             <img src={item.image} />
             <h3>{item.name}</h3>
             <p>category : {item.category}</p>
